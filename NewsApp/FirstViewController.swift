0438734096f59612ee7bc690211262fd182dc215
Francis Ngunjiri <fngunjiri@shimba.co.ke>
2019-06-12 19:15:20 +0300

    Initial Commit

---
+++ NewsApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NewsApp
+//
+//  Created by Francis Ngunjiri on 12/06/2019.
+//  Copyright © 2019 Francis Ngunjiri. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
